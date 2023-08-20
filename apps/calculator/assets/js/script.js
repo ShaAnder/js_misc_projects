2a0f21a536b7b17753b413937c408a8b89d411e3
ShaAnder <shaunanderton1@gmail.com>
2023-08-20 13:21:27 +0100

    Second sweep of refactoring

--- apps/calculator/assets/js/script.js
+++ apps/calculator/assets/js/script.js
@@ -1,34 +1,30 @@
 "use strict";
 
+// SET VARIABLES
 const actions = document.querySelector(".actions");
-
 const ans = document.querySelector(".ans");
 
 let expression = "";
 let a = 0;
 
-const square = () => {
-  return expression * expression;
-};
-
 const calcFunctions = {
-  "x^2": () => square(),
   radic: (expression) => Math.sqrt(expression),
   log: (expression) => Math.log(expression),
   cos: (expression) => Math.cos(expression),
   sin: (expression) => Math.sin(expression),
   tan: (expression) => Math.tan(expression),
   exp: (expression) => Math.exp(expression),
-  "%": (expression) => Math.floor(expression),
   ce: function calcClear() {
     expression = "";
     ans.value = 0;
+  },
+  "x^2": function square() {
+    return expression * expression;
   },
 };
 
 actions.addEventListener("click", (event) => {
   const value = event.target.dataset["value"];
-
   if (value !== undefined) {
     if (value in calcFunctions) {
       expression = calcFunctions[value]?.(expression);
@@ -45,3 +41,5 @@ actions.addEventListener("click", (event) => {
     }
   }
 });
+
+// TODO -> Inv, IN, E, ABC, FUNC
